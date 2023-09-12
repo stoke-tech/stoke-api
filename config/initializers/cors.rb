8985572476e37911063aec88d4d5383d55f286a7
Zach <zdb1994@yahoo.com>
2023-09-11 21:35:16 -0400

    Adds frontend to CORS policy.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001"
+    origins 'http://localhost:3001', 'https://stoketech.netlify.app'
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post]
+    resource '*', headers: :any, methods: %i[get post]
   end
 end
